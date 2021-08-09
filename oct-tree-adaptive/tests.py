3c984e090911ab7347c79fcae7e17a3403e3835b
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-08-09 19:56:17 +0530

    all tests done for now

--- oct-tree-adaptive/tests.py
+++ oct-tree-adaptive/tests.py
@@ -1,6 +1,7 @@
 from tree_compyle import *
 
-backend='cython'
+backend = 'cython'
+
 
 def test_copy_arr():
     arr = ary.ones(10, dtype=np.int32, backend=backend)
@@ -32,17 +33,108 @@ def test_internal_nodes():
     r_lca_level = np.array([1], dtype=np.int32)
     r_lca_idx = np.array([-1], dtype=np.int32)
     (sfc, level, r_lca_sfc, r_lca_level,
-    r_lca_idx) = wrap(sfc, level, r_lca_sfc, 
-                      r_lca_level, r_lca_idx,
-                      backend=backend)
+     r_lca_idx) = wrap(sfc, level, r_lca_sfc,
+                       r_lca_level, r_lca_idx,
+                       backend=backend)
 
     lca_sfc = ary.empty(1, dtype=np.int32, backend=backend)
     lca_level = ary.empty(1, dtype=np.int32, backend=backend)
     lca_idx = ary.empty(1, dtype=np.int32, backend=backend)
-    
+
     e = Elementwise(internal_nodes, backend=backend)
     e(sfc[:-1], sfc[1:], level[:-1], level[1:], lca_sfc, lca_level, lca_idx)
 
-    (np.testing.assert_array_equal(r_lca_sfc, lca_sfc) and 
-    np.testing.assert_array_equal(r_lca_level, lca_level) and 
-    np.testing.assert_array_equal(r_lca_idx, lca_idx))
+    (np.testing.assert_array_equal(r_lca_sfc, lca_sfc) and
+     np.testing.assert_array_equal(r_lca_level, lca_level) and
+     np.testing.assert_array_equal(r_lca_idx, lca_idx))
+
+
+def test_parent_child():
+    sfc = np.array([52, 53], dtype=np.int32)
+    level = np.array([2, 2], dtype=np.int32)
+    r_lca_sfc = np.array([6], dtype=np.int32)
+    r_lca_level = np.array([1], dtype=np.int32)
+    r_lca_idx = np.array([-1], dtype=np.int32)
+    r_child_sfc = np.array([52], dtype=np.int32)
+    r_child_idx = np.array([0], dtype=np.int32)
+    (sfc, level, r_lca_sfc, r_lca_level, r_lca_idx, r_child_sfc,
+     r_child_idx) = wrap(sfc, level, r_lca_sfc, r_lca_level,
+                         r_lca_idx, r_child_sfc, r_child_idx,
+                         backend=backend)
+
+    lca_sfc = ary.empty(1, dtype=np.int32, backend=backend)
+    lca_level = ary.empty(1, dtype=np.int32, backend=backend)
+    lca_idx = ary.empty(1, dtype=np.int32, backend=backend)
+    child_sfc = ary.empty(1, dtype=np.int32, backend=backend)
+    child_idx = ary.empty(1, dtype=np.int32, backend=backend)
+
+    e = Elementwise(parent_child, backend=backend)
+    e(sfc[:-1], sfc[1:], level[:-1], level[1:],
+      lca_sfc, lca_level, lca_idx, child_sfc, child_idx)
+
+    np.testing.assert_array_equal(r_lca_sfc, lca_sfc)
+    np.testing.assert_array_equal(r_lca_level, lca_level)
+    np.testing.assert_array_equal(r_lca_idx, lca_idx)
+    np.testing.assert_array_equal(r_child_sfc, child_sfc)
+    np.testing.assert_array_equal(r_child_idx, child_idx)
+
+
+def test_sfc_same():
+    sfc = np.array([52, 6], dtype=np.int32)
+    level = np.array([2, 1], dtype=np.int32)
+    cpy_sfc = np.array([52, 6], dtype=np.int32)
+    sfc, level, cpy_sfc = wrap(
+        sfc, level, cpy_sfc, backend=backend)
+    max_level = 2
+
+    e = Elementwise(sfc_same, backend=backend)
+    e(sfc, level, max_level)
+
+    np.testing.assert_array_equal(cpy_sfc, sfc)
+
+
+def test_sfc_real():
+    sfc = np.array([52, 48], dtype=np.int32)
+    level = np.array([2, 1], dtype=np.int32)
+    cpy_sfc = np.array([52, 6], dtype=np.int32)
+    sfc, level, cpy_sfc = wrap(
+        sfc, level, cpy_sfc, backend=backend)
+    max_level = 2
+
+    e = Elementwise(sfc_real, backend=backend)
+    e(sfc, level, max_level)
+
+    np.testing.assert_array_equal(cpy_sfc, sfc)
+
+
+def test_id_duplicates():
+    sfc = np.array([52, 52, 53], dtype=np.int32)
+    level = np.array([2, 2, 2], dtype=np.int32)
+    r_duplicate_idx = np.array([0, 1], dtype=np.int32)
+
+    sfc, level, r_duplicate_idx = wrap(
+        sfc, level, r_duplicate_idx, backend=backend)
+
+    duplicate_idx = ary.empty(2, dtype=np.int32, backend=backend)
+
+    e = Elementwise(id_duplicates, backend=backend)
+    e(sfc, level, duplicate_idx)
+
+    np.testing.assert_array_equal(r_duplicate_idx, duplicate_idx)
+
+
+def test_remove_duplicates():
+    sfc = np.array([52, 52, 53], dtype=np.int32)
+    level = np.array([2, 2, 2], dtype=np.int32)
+    duplicate_idx = np.array([0, 1], dtype=np.int32)
+    r_sfc = np.array([52, 0, 53], dtype=np.int32)
+    r_level = np.array([2, -1, 2], dtype=np.int32)
+
+    sfc, level, duplicate_idx, r_sfc, r_level = wrap(
+        sfc, level, duplicate_idx, r_sfc, r_level, backend=backend)
+
+    e = Elementwise(remove_duplicates, backend=backend)
+    e(duplicate_idx, sfc, level)
+
+    np.testing.assert_array_equal(r_sfc, sfc)
+    np.testing.assert_array_equal(r_level, level)
