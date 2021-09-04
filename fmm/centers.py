8a7693704cd9de26a85042fa949f9201e4a23cf6
Harsh <harshbaldwa@gmail.com>
2021-09-04 16:48:44 +0530

    Ready for AFMM implementation

--- fmm/centers.py
+++ fmm/centers.py
@@ -3,6 +3,7 @@ import compyle.array as ary
 import numpy as np
 import time
 from .tree import build
+from .spherical_points import spherical_points
 
 
 @annotate(x="int", return_="int")
@@ -49,6 +50,50 @@ def setting_p2(i, cx, cy, cz, out_x, out_y,
         in_z[i*num_p2+j] = cz[i]+sph_pts[3*j+2]*0.5*sz_cell
 
 
+def set_prob(N, max_depth, part_x, part_y, part_z, x_min,
+             y_min, z_min, length, num_p2, backend):
+
+    # TODO: collect all the declarations in a single
+    #       list in a single file
+    cells, sfc, level, idx, parent, child = build(
+        N, max_depth, part_x, part_y, part_z, x_min,
+        y_min, z_min, length, backend)
+
+    cx = ary.zeros(cells, dtype=np.float32, backend=backend)
+    cy = ary.zeros(cells, dtype=np.float32, backend=backend)
+    cz = ary.zeros(cells, dtype=np.float32, backend=backend)
+    out_x = ary.zeros(cells*num_p2, dtype=np.float32,
+                      backend=backend)
+    out_y = ary.zeros(cells*num_p2, dtype=np.float32,
+                      backend=backend)
+    out_z = ary.zeros(cells*num_p2, dtype=np.float32,
+                      backend=backend)
+    out_vl = ary.zeros(cells*num_p2, dtype=np.float32,
+                       backend=backend)
+    in_x = ary.zeros(cells*num_p2, dtype=np.float32,
+                     backend=backend)
+    in_y = ary.zeros(cells*num_p2, dtype=np.float32,
+                     backend=backend)
+    in_z = ary.zeros(cells*num_p2, dtype=np.float32,
+                     backend=backend)
+    in_vl = ary.zeros(cells*num_p2, dtype=np.float32,
+                      backend=backend)
+
+    sph_pts, order = spherical_points(N)
+
+    ecalc_center = Elementwise(calc_center, backend=backend)
+    esetting_p2 = Elementwise(setting_p2, backend=backend)
+
+    ecalc_center(sfc, level, cx, cy, cz,
+                 x_min, y_min, z_min, length)
+    esetting_p2(cx, cy, cz, out_x, out_y, out_z, in_x,
+                in_y, in_z, sph_pts, length, level, num_p2)
+
+    return sfc, level, idx, parent, child, cx, cy, cz, \
+        out_x, out_y, out_z, out_vl, in_x, in_y, in_z, \
+        in_vl, sph_pts, order
+
+
 if __name__ == "__main__":
     backend = 'cython'
     N = 10
@@ -57,18 +102,15 @@ if __name__ == "__main__":
     y_min = 0
     z_min = 0
     length = 1
+    num_p2 = 6
     np.random.seed(4)
     part_x = np.random.random(N)*length + x_min
     part_y = np.random.random(N)*length + y_min
     part_z = np.random.random(N)*length + z_min
 
-    cells, sfc, level, idx, parent, child = build(
-        N, max_depth, part_x, part_y, part_z, x_min,
-        y_min, z_min, length, backend)
-
-    cx = ary.zeros(cells, dtype=np.float64, backend=backend)
-    cy = ary.zeros(cells, dtype=np.float64, backend=backend)
-    cz = ary.zeros(cells, dtype=np.float64, backend=backend)
-
-    e = Elementwise(calc_center, backend=backend)
-    e(sfc, level, cx, cy, cz, x_min, y_min, z_min, length)
+    sfc, level, idx, parent, child, cx, cy, cz, \
+        out_x, out_y, out_z, out_vl, in_x, in_y, \
+        in_z,  in_vl, sph_pts, order = set_prob(
+            N, max_depth, part_x, part_y, part_z,
+            x_min, y_min, z_min, length, num_p2,
+            backend)
