25b84346fa7cd441244431b2e5f61ec18998b53e
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-11-28 10:24:13 +0530

    Minor Changes

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 
 classes = '''
+Development Status :: 2 - Pre-Alpha
 Intended Audience :: Developers
 Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
 Natural Language :: English
-Operating System :: OS Independent
-Programming Language :: Python
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
 Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Topic :: Utilities
 '''
 classifiers = [x.strip() for x in classes.splitlines() if x]
+
+install_requires = ['scipy', 'pyyaml', 'compyle']
+tests_require = ['pytest']
+docs_require = ['sphinx', 'sphinx-copybutton']
 
 setup(
     name="fmmpy",
@@ -19,11 +29,14 @@ setup(
     long_description=open('README.rst').read(),
     url="https://github.com/harshbaldwa/fmmpy",
     packages=find_packages(),
+    license="MIT",
     package_data={
         'data': ['fmmpy/data/']
     },
     classifiers=classifiers,
-    setup_requires=[
-        "flake8"
-    ]
+    install_requires=install_requires,
+    extras_require={
+        "docs": docs_require,
+        "tests": tests_require,
+    }
 )
